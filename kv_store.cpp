b963c64836d695c88ddd36069b4c1c4ccd458a74
Justin Innis <justininnis988@gmail.com>
2026-09-14 11:52:51 -0400

    Add basic bucket structure

---
+++ kv_store.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const int NUM_BUCKETS = 10;
+
+int main() {
+    cout << "kv-store starting up" << endl;
+    return 0;
+}
